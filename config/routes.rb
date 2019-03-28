f6a059b5b32ec205d5cdaea1d486fb247891011b
Nguyen Hieu <hieukk25021991@gmail.com>
2019-03-28 18:01:34 +0700

    password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   default_url_options :host => "localhost"
   root 'static_pages#home'
   get 'sessions/new'
@@ -17,5 +21,6 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
 end
