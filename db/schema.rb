d52190bb9adf390d705db21a5b706e37c50e6bd9
Nguyen Hieu <hieukk25021991@gmail.com>
2019-04-22 20:17:29 +0700

    a

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,17 @@ ActiveRecord::Schema.define(version: 20190328130456) do
     t.string "picture"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
+end
+ActiveRecord::Schema.define(version: 20190422115707) do
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
